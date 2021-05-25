56be0c21e11ed75684f190a1b039397f8a293dd1
236507 Radoslaw Bucki <236507@edu.p.lodz.pl>
2021-05-25 16:01:40 +0200

    ClientManager

---
+++ project/library/include/ClientManager.cpp
@@ -0,0 +1,30 @@
+#include "ClientManager.h"
+#include "Client.h"
+#include "typedefs.h"
+#include <boost/date_time.hpp>
+
+ClientManager::ClientManager(const ClientRepositoryPtr &clientRepository) : clientRepository(clientRepository) {}
+
+ClientPtr ClientManager::getClient(std::string personalId) {
+    return ClientPtr();
+}
+
+void ClientManager::addClient(const std::string &personalId, const std::string &firstName, const std::string &lastName,
+                              const boost::posix_time::ptime &birthDate) {
+    if (clientRepository->getClient(personalId) != nullptr){
+        ClientPtr client = std::make_shared<Client>(personalId,firstName,lastName,birthDate);
+        clientRepository->addClient(client);
+    }
+}
+
+bool predicateTrueManager(ClientPtr client){
+    return client != nullptr;
+}
+
+std::vector<ClientPtr> ClientManager::findAll() {
+    return findClients(predicateTrueManager);
+}
+
+std::vector<ClientPtr> ClientManager::findClients(ClientPredicate predicate) {
+    return findClients(predicate);
+}
---
+++ project/library/include/ClientManager.h
@@ -0,0 +1,22 @@
+#ifndef BANK_CLIENTMANAGER_H
+#define BANK_CLIENTMANAGER_H
+
+#include "ClientRepository.h"
+#include <boost/date_time.hpp>
+
+class ClientManager {
+private:
+    ClientRepositoryPtr clientRepository;
+public:
+    ///CONSTRUCTOR
+    ClientManager(const ClientRepositoryPtr &clientRepository);
+    ///METHODS
+    ClientPtr getClient(std::string personalId);
+    void addClient(const std::string &personalId, const std::string &firstName, const std::string &lastName,
+                   const boost::posix_time::ptime &birthDate);
+    std::vector<ClientPtr> findAll();
+    std::vector<ClientPtr> findClients(ClientPredicate predicate);
+};
+
+
+#endif //BANK_CLIENTMANAGER_H
