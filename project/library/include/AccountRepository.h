81c3a8f7cb502edd02b230c3dabb0f59b6a8c265
236500 Bednarek Kamil <236500@edu.p.lodz.pl>
2021-05-25 14:18:19 +0200

    AccountRepository

---
+++ project/library/include/AccountRepository.h
@@ -0,0 +1,25 @@
+//
+// Created by student on 25.05.2021.
+//
+
+#ifndef BANK_ACCOUNTREPOSITORY_H
+#define BANK_ACCOUNTREPOSITORY_H
+
+#include <vector>
+#include "typedefs.h"
+
+class AccountRepository {
+private:
+    std::vector<AccountPtr> accounts;
+public:
+    ///CONTRUCTOR
+    AccountRepository();
+    ///METHODS
+    AccountPtr getAccount(std::string accountNumber);
+    void addAccount(AccountPtr account);
+    bool removeAccount(AccountPtr account);
+    std::vector<AccountPtr> findBy(AccountPredicate predicate);
+};
+
+
+#endif //BANK_ACCOUNTREPOSITORY_H
---
+++ project/library/src/AccountRepository.cpp
@@ -0,0 +1,42 @@
+//
+// Created by student on 25.05.2021.
+//
+
+#include "AccountRepository.h"
+#include "Account.h"
+
+AccountRepository::AccountRepository() {}
+
+AccountPtr AccountRepository::getAccount(std::string accountNumber) {
+    for (int i = 0; i < accounts.size(); i++) {
+        if (accounts[i]->getAccountNumber() == accountNumber)
+            return accounts[i];
+    }
+    return nullptr;
+}
+
+void AccountRepository::addAccount(AccountPtr account) {
+    if (account != nullptr)
+        accounts.push_back(account);
+}
+
+bool AccountRepository::removeAccount(AccountPtr account) {
+    for (int i = 0; i < accounts.size(); i++) {
+        if (accounts[i] == account){
+            accounts.erase(std::remove(accounts.begin(),accounts.end(),account));
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<AccountPtr> AccountRepository::findBy(AccountPredicate predicate) {
+    std::vector<AccountPtr> found;
+    for (int i = 0; i < accounts.size(); i++) {
+        AccountPtr account = accounts[i];
+        if (account != nullptr && predicate(account)) {
+            found.push_back(account);
+        }
+    }
+    return found;
+}
