6af6079059590a410a318afa0cc996911270d4a3
236507 Radoslaw Bucki <236507@edu.p.lodz.pl>
2021-06-04 16:34:58 +0200

    Account ExceptionTest

--- project/program/test/ExceptionsTest.cpp
+++ project/program/test/ExceptionsTest.cpp
@@ -2,7 +2,10 @@
 #include <boost/date_time/posix_time/ptime.hpp>
 #include "typedefs.h"
 #include "model/Client.h"
+#include "model/CurrentAccount.h"
+#include "model/SavingsAccount.h"
 #include "exceptions/ClientException.h"
+#include "exceptions/AccountException.h"
 
 BOOST_AUTO_TEST_SUITE(ExceptionsTest)
 
@@ -21,6 +24,25 @@ BOOST_AUTO_TEST_SUITE(ExceptionsTest)
                              boost::posix_time::ptime(boost::gregorian::date(2000,5,13))),ClientException::exception);
         BOOST_CHECK_THROW(ClientPtr clientError = std::make_shared<Client>("01234567891", "Marcin", "Nowak",
                                   boost::posix_time::second_clock::local_time()),ClientException::exception);
+    }
+
+    BOOST_AUTO_TEST_CASE(CurrentAccountExceptionTest) {
+        ClientPtr owner = std::make_shared<Client>("01234567891", "Marcin", "Nowak",
+                                                   boost::posix_time::ptime(boost::gregorian::date(2000, 5, 13)));
+        BOOST_CHECK_NO_THROW(CurrentAccountPtr currentAccountError = std::make_shared<CurrentAccount>(owner, 1));
+        BOOST_CHECK_THROW(CurrentAccountPtr currentAccountError = std::make_shared<CurrentAccount>(nullptr, 1),AccountException::exception);
+        BOOST_CHECK_THROW(CurrentAccountPtr currentAccountError = std::make_shared<CurrentAccount>(owner, 9000),AccountException::exception);
+        BOOST_CHECK_THROW(CurrentAccountPtr currentAccountError = std::make_shared<CurrentAccount>(owner, -1),AccountException::exception);
+    }
+    BOOST_AUTO_TEST_CASE(SavingsAccountExceptionTest) {
+        ClientPtr owner = std::make_shared<Client>("01234567891", "Marcin", "Nowak",
+                                                   boost::posix_time::ptime(boost::gregorian::date(2000,5,13)));
+        CurrentAccountPtr acc = std::make_shared<CurrentAccount>(owner,1);
+        BOOST_CHECK_NO_THROW(SavingsAccountPtr savacc = std::make_shared<SavingsAccount>(owner,acc,2));
+        BOOST_CHECK_THROW(SavingsAccountPtr savacc = std::make_shared<SavingsAccount>(nullptr,acc,2),AccountException::exception);
+        BOOST_CHECK_THROW(SavingsAccountPtr savacc = std::make_shared<SavingsAccount>(owner,nullptr,2),AccountException::exception);
+        BOOST_CHECK_THROW(SavingsAccountPtr savacc = std::make_shared<SavingsAccount>(owner,acc,9000),AccountException::exception);
+        BOOST_CHECK_THROW(SavingsAccountPtr savacc = std::make_shared<SavingsAccount>(owner,acc,-1),AccountException::exception);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
